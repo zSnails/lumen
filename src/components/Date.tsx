33056b390945dbba73f84ef06f87e75d07756ae1
zSnails <z@gonzaleza.cr>
2026-06-24 16:33:16 -0600

    feat: make the date component functional

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,5 +1,13 @@
 export function Date() {
+    const date = Temporal.Now.plainDateISO().toLocaleString(
+        'en-US',
+        {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric',
+        },
+    );
     return <div className="text-xl font-sans text-steel">
-        Saturday, June 20
+        {date}
     </div>
 }
