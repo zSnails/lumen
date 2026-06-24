355a23cb38e8f49bd5cd13abfb3771dbe44ec766
zSnails <z@gonzaleza.cr>
2026-06-24 16:50:25 -0600

    feat: make the greeting component functional

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,5 +1,20 @@
+function getGreeting(hour: number): string {
+    switch (true) {
+        case (hour >= 22 && hour <= 23 || hour >= 0 && hour <= 4):
+            return "Goodnight.";
+        case (hour >= 5 && hour <= 11):
+            return "Good morning.";
+        case (hour >= 12 && hour <= 16):
+            return "Good afternoon.";
+        case (hour >= 17 && hour <= 21):
+            return "Good evening.";
+        default:
+            return "Hello.";
+    }
+}
+
 export function Greeting() {
     return <div className="tracking-widest text-steel">
-        Good Afternoon
+        {getGreeting(Temporal.Now.plainTimeISO().hour)}
     </div>;
 }
